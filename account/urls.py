aa8a4a7ace0750561568a918465e50ec853525c3
GM <gm.mythilig@gmail.com>
2025-11-06 19:24:28 +0530

    urls source

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
