84f4669253fb9071362a289a160000457341c5e2
gm.mythilig@gmail.com <gm.mythilig@gmail.com>
2025-11-06 19:39:03 +0530

    set updated

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -127,3 +127,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INSTALLED_APPS = [
+    ...
+    'rest_framework',
+    'rest_framework.authtoken',
+]
