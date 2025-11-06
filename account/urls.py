8014fd0740c41906612b2941699acfece5d4a060
gm.mythilig@gmail.com <gm.mythilig@gmail.com>
2025-11-06 19:26:00 +0530

    models data source

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import *
 
 urlpatterns = [
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    
 ]
